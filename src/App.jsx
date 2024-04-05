d12d9c680afb569673f62b04d171866db0d60da2
benprice2 <ben.price700@gmail.com>
2024-04-06 12:44:30 +1300

    about section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import styles from './App.module.css'
 import { Navbar } from './components/nav/Navbar';
 import { Hero } from './components/hero/Hero';
+import { About } from './components/about/About';
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <div className={styles.App}>
       <Navbar />
       <Hero />
+      <About />
     </div>
 
   )
